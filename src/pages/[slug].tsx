177059529cee6a93d9fcc388b31bf66f1bcf52fd
Ryohei <harrry.kuma1225@gmail.com>
2023-04-27 00:11:58 +0900

    commit

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -26,14 +26,26 @@ query Post($slug:String!){
 `
 const Post = ({data}:PostData) => {
   return (
-    <div>
-      <p>{data.title}</p>
-      <p dangerouslySetInnerHTML={{ __html: data.content.html }}></p>
-      <p></p>
+    <div className="bg-gray-100 min-h-screen">
+      <header className="bg-indigo-600 shadow-md py-4">
+        <div className="container mx-auto px-4">
+          <h1 className=" text-3xl font-bold text-gray-900">My Blog</h1>
+        </div>
+      </header>
+      <div className="container mx-auto px-4 py-8">
+        <div className="bg-white shadow-md p-4">
+          <h2 className="text-4xl font-bold mb-4">{data.title}</h2>
+          <div
+            className="prose max-w-none"
+            dangerouslySetInnerHTML={{ __html: data.content.html }}
+          ></div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 export default Post
+
 
 export async function getStaticProps({params}:any) {
   const variables = { slug: params.slug }; 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,18 +42,32 @@ export interface Data{
 }
 
 export default function Home({data}:Data) {
-  console.log(data);
-  return (
-    <div>
-      {data.map((data:Posts) =>(
-        <div key={data.id}>
-          <Link href={`/${data.slug}`}>
-          <h1>{data.title}</h1>
-          </Link>
+  return  (
+    <div className="bg-gray-100 min-h-screen">
+      <header className="bg-indigo-600 py-4">
+        <div className="container mx-auto px-4">
+          <h1 className="text-3xl font-bold text-gray-900">My Blog</h1>
         </div>
-      ))}
+      </header>
+      <div className="container mx-auto px-4 py-8">
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {data.map((data: Posts) => (
+            <div
+              key={data.id}
+              className="bg-white shadow-md p-4 rounded-lg hover:shadow-xl transition duration-300"
+            >
+              <Link href={`/${data.slug}`}>
+                <span>
+                  <h2 className="text-xl font-bold text-gray-800 mb-2">{data.title}</h2>
+                  <p className="text-gray-600">{data.datePublished}</p>
+                </span>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 export async function getStaticProps() {
   const data:Props = await request('https://api-ap-northeast-1.hygraph.com/v2/clgvphora0uuc01uefl0l1adb/master',QUERY)
